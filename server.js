f39c0139b0afba48353660172a48d43138475fa2
Juha Yoon <ijooha16@gmail.com>
2025-02-25 17:14:41 +0900

    fix: 재설정

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-import jsonServer from "json-server";
-
+// server.js
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
